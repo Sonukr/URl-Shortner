111863a4ebf223bb9d0ef1501e1b1cabbd22af91
Sonukr <sonuyadav9756@gmail.com>
2017-05-21 01:07:47 +0530

    validation added for url

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 $('.btn-shorten').on('click', function(){
   var input = $('#url-field').val();
-  if(input != ""){
+  if(input != "" && ValidURL(input)){
     $.ajax({
       url: '/api/shorten',
       type: 'POST',
@@ -13,10 +13,27 @@ $('.btn-shorten').on('click', function(){
           $('#link').hide().fadeIn('slow');
       }
     });
-  }else{
-      alert("Please Enter a url")
+  }else if(input == "" ){
+      alert("Please enter a url")
   }
-
-
-
 });
+
+$('#url-field').on('keypress', function(e){
+  if(e.keyCode === 13){
+    $('.btn-shorten').click();
+  }
+});
+
+
+function ValidURL(str) {
+  var pattern = new RegExp('^(?:(ftp|http|https):\/\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\d+)?(\\//[-a-z\\d%_.~+]*)*'); // port and pat
+  if(!pattern.test(str)) {
+    alert("Please enter a valid URL.");
+    return false;
+  } else {
+    return true;
+  }
+}
