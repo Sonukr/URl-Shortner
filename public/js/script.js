0cca28ed8295272ab68de1dde3dcdfdd02d45a8c
Sonukr <sonuyadav9756@gmail.com>
2017-05-21 00:21:02 +0530

    url http method fixed

--- public/js/script.js
+++ public/js/script.js
@@ -7,7 +7,7 @@ $('.btn-shorten').on('click', function(){
       dataType: 'JSON',
       data: {url: $('#url-field').val()},
       success: function(data){
-          var resultHTML = '<a class="result" href="' + data.shortUrl + '">'
+          var resultHTML = '<a class="result" href="http://' + data.shortUrl + '">'
               + data.shortUrl + '</a>';
           $('#link').html(resultHTML);
           $('#link').hide().fadeIn('slow');
